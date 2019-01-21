e2cf6fcfdc6669b71bb742efb6e4b7cefbe75586
Job Jacob <jobjacob98@gmail.com>
2019-01-21 12:36:21 +0530

    Add files via upload

---
+++ Bluetooth Controlled Bulbs/bluetooth_bulb.ino
@@ -0,0 +1,35 @@
+void setup()
+{
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  int x = Serial.read();
+  if(x == 1)
+  {
+    digitalWrite(5, HIGH);
+  }
+  else if(x == 5)
+  {
+    digitalWrite(5, LOW);
+  }
+  if(x == 2)
+  {
+    digitalWrite(6, HIGH);
+  }
+  else if(x == 6)
+  {
+    digitalWrite(6, LOW);
+  }
+  if(x == 3)
+  {
+    digitalWrite(7, HIGH);
+  }
+  else if(x == 7)
+  {
+    digitalWrite(7, LOW);
+  }  
+}
