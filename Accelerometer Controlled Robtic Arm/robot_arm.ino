a012c857009e8956516d03b7a735081d300f7614
Job Jacob <jobjacob98@gmail.com>
2019-01-21 13:17:26 +0530

    Add files via upload

---
+++ Accelerometer Controlled Robtic Arm/robot_arm.ino
@@ -0,0 +1,137 @@
+/*
+ ADXL3xx
+ 
+ Reads an Analog Devices ADXL3xx accelerometer and communicates the
+ acceleration to the computer.  The pins used are designed to be easily
+ compatible with the breakout boards from Sparkfun, available from:
+ http://www.sparkfun.com/commerce/categories.php?c=80
+
+ http://www.arduino.cc/en/Tutorial/ADXL3xx
+
+ The circuit:
+ analog 0: accelerometer self test
+ analog 1: z-axis
+ analog 2: y-axis
+ analog 3: x-axis
+ analog 4: ground
+ analog 5: vcc
+ 
+ created 2 Jul 2008
+ by David A. Mellis
+ modified 30 Aug 2011
+ by Tom Igoe 
+ 
+ This example code is in the public domain.
+
+*/
+
+// these constants describe the pins. They won't change:
+#include<Servo.h>
+
+const int groundpin = 18;             // analog input pin 4 -- ground
+const int powerpin = 19;              // analog input pin 5 -- voltage
+/*const int xpin = A3;                  // x-axis of the accelerometer
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis (only on 3-axis models)*/
+
+Servo servox;
+Servo servoy;
+Servo servoz;
+Servo servog;
+
+int posx = 90;
+int posy = 90;
+int posz = 40;
+int posg = 0;
+
+int x = A3;
+int y = A2;
+int z = A1;
+
+int valx = 0;
+int valy = 0;
+int valz = 0;
+
+void setup()
+{
+  // initialize the serial communications:
+  
+  
+  // Provide ground and power by using the analog inputs as normal
+  // digital pins.  This makes it possible to directly connect the
+  // breakout board to the Arduino.  If you use the normal 5V and
+  // GND pins on the Arduino, you can remove these lines.
+  pinMode(groundpin, OUTPUT);
+  pinMode(powerpin, OUTPUT);
+  digitalWrite(groundpin, LOW); 
+  digitalWrite(powerpin, HIGH);
+  
+  servox.attach(9);
+  servoy.attach(10);
+  servoz.attach(11);
+  servog.attach(6);
+  
+  servox.write(90);
+  servoy.write(90);
+  servoz.write(40);
+  servog.write(0);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  valx = analogRead(x);
+  valy = analogRead(y);
+  valz = analogRead(z);
+
+  // print the sensor values:
+  Serial.print(analogRead(x));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(y));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(z));
+  Serial.println();
+  // delay before next reading:
+  delay(35);
+  
+  if((valx>270)&&(valx<315)&&(valy>320)&&(valy<350))
+  {
+    posx--;
+    servox.write(posx);
+  }
+    if((valx>375)&&(valx<415)&&(valy>320)&&(valy<350))
+  {
+    posx++;
+    servox.write(posx);
+  }
+    if((valx>320)&&(valx<350)&&(valy>260)&&(valy<300))
+  {
+    posy++;
+    servoy.write(posy);
+  }
+    if((valx>320)&&(valx<360)&&(valy>370)&&(valy<400))
+  {
+    posy--;
+    servoy.write(posy);
+  }
+  if(valz<330)
+  {
+    servog.write(180);
+  }
+  if(valz>350)
+  {
+    servog.write(0);
+  }
+    if((valx>290)&&(valx<315)&&(valy>305)&&(valy<330))
+  {
+    posz++;
+    servoz.write(posz);
+  }
+    if((valx>350)&&(valx<380)&&(valy>305)&&(valy<330))
+  {
+    posz--;
+    servoz.write(posz);
+  }
+}
